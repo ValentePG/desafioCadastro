665531f05dcf55e2f22b064ac4e2c218aad9a934
Valente <bielvhv3@gmail.com>
2025-03-16 18:36:01 -0300

    feature: Inicio do desenvolvimento da função cadastrar pet

--- src/main/java/dev/valente/desafiocadastro/RegisterForm.java
+++ src/main/java/dev/valente/desafiocadastro/RegisterForm.java
@@ -3,6 +3,7 @@ package dev.valente.desafiocadastro;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class RegisterForm {
 
@@ -31,6 +32,31 @@ public class RegisterForm {
             System.out.println(value);
         });
         br.close();
+    }
+
+//    public void adicionarPergunta(String pergunta) throws IOException {
+//        File fw = new File("src/main/resources/formulario.txt");
+//        BufferedWriter bw = new BufferedWriter(new FileWriter(fw, true));
+//        bw.write("\n" + (count+1) + " - " + pergunta);
+//        bw.close();
+//    }
+
+    public void cadastrarPet(int choose){
+        Scanner sc = new Scanner(System.in);
+        Pet newPet = new Pet();
+        String c;
+        int n;
+        if(choose == 1){
+            System.out.println(map.get(choose));
+            c = sc.nextLine();
+            newPet.setNome(c);
+        }
+        if(choose == 2){
+            System.out.println("Digite 1 para cachorro ou 2 para gato: ");
+            n = sc.nextInt();
+            newPet.setTipo(n);
+        }
+        System.out.println(newPet);
     }
 
     public Map<Integer, String> getMap() {

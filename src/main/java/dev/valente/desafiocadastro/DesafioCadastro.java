eef6c2300e1ccc2da37c7140db46062cff3781a0
Valente <bielvhv3@gmail.com>
2025-03-17 18:00:38 -0300

    feature: Adicionando tratamento de exceções a opção 1 do programa e mudando estrutura de repetição para do while

--- src/main/java/dev/valente/desafiocadastro/DesafioCadastro.java
+++ src/main/java/dev/valente/desafiocadastro/DesafioCadastro.java
@@ -1,26 +1,37 @@
 package dev.valente.desafiocadastro;
 
+import dev.valente.desafiocadastro.entidade.Pet;
+import dev.valente.desafiocadastro.service.menu.Menu;
+import dev.valente.desafiocadastro.service.petregistration.RegisterForm;
+import dev.valente.desafiocadastro.util.ScannerUtils;
+
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DesafioCadastro {
     public static void main(String[] args) throws IOException {
-
+        Scanner sc = new Scanner(System.in);
         int input = 0;
-        while (input != 6){
-
-            Menu menu = new Menu();
-            System.out.print("Escolha sua opção: ");
-            Scanner sc = new Scanner(System.in);
-            input = sc.nextInt();
-            System.out.println("======================================");
-            if (input == 1) {
-                RegisterForm registerForm = new RegisterForm();
-                System.out.print("Escolha qual informação quer cadastrar primeiro: ");
+        do{
+            Menu.showMenu();
+            System.out.print("Escolha uma opção: ");
+            try{
                 input = sc.nextInt();
-                registerForm.cadastrarPet(input);
-                System.out.println("===================================");
+                ScannerUtils.cleanBuffer(sc);
+                if(!(input >= 0)){
+                    throw new InputMismatchException();
+                }
+                System.out.println("======================================");
+                if (input == 1) {
+                    RegisterForm registerForm = new RegisterForm();
+                    Pet novoPet = registerForm.registerPet(sc);
+                    registerForm.showPet(novoPet);
+                }
+            }catch (InputMismatchException ex){
+                ScannerUtils.cleanBuffer(sc);
+                System.out.println("Por favor insira um número válido!");
             }
-        }
+        } while (input != 6);
     }
 }

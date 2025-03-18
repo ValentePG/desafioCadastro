63ae35b1439527207a4324661b28dda84f499e13
Valente <bielvhv3@gmail.com>
2025-03-18 20:54:46 -0300

    feature: Adicionando loop para salvar informação, adicionando função de parsing

--- src/main/java/dev/valente/desafiocadastro/service/petregistration/WeightPet.java
+++ src/main/java/dev/valente/desafiocadastro/service/petregistration/WeightPet.java
@@ -1,7 +1,6 @@
 package dev.valente.desafiocadastro.service.petregistration;
 
 import dev.valente.desafiocadastro.entidade.Pet;
-import dev.valente.desafiocadastro.util.ScannerUtils;
 
 import java.util.Scanner;
 
@@ -9,11 +8,24 @@ public record WeightPet(String question) implements PetRegistrationOptions {
 
     @Override
     public void registerPetInfo(Pet pet, Scanner input) {
-        float c;
-        c = input.nextFloat();
-        ScannerUtils.cleanBuffer(input);
-        pet.setWeight(c);
-        System.out.println("Informação salva: " + pet.getWeight());
+        float weight;
+        String userInfoInput;
+        String out = "n";
+        do{
+            try{
+                System.out.println(getQuestion());
+                userInfoInput = input.nextLine();
+                assertMatchesWithRegex(userInfoInput);
+                userInfoInput = userInfoInput.replace(",", ".");
+                weight = Float.parseFloat(userInfoInput);
+                assertWeightIsValid(weight);
+                pet.setWeight(weight);
+                System.out.println("Deseja salvar esta informação?(S/N): " + pet.getWeight());
+                out = input.nextLine();
+            }catch (RuntimeException e){
+                System.out.println(e.getMessage());
+            }
+        }while (!out.equalsIgnoreCase("S"));
 
     }
 

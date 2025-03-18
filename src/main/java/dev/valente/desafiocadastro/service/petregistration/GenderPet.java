bf2f5abc4b5c67a8846f9f66a89c164e7f006dd0
Valente <bielvhv3@gmail.com>
2025-03-18 19:25:30 -0300

    feature: Adicionando loop para salvar informação e tratamento de exceção.

--- src/main/java/dev/valente/desafiocadastro/service/petregistration/GenderPet.java
+++ src/main/java/dev/valente/desafiocadastro/service/petregistration/GenderPet.java
@@ -3,6 +3,7 @@ package dev.valente.desafiocadastro.service.petregistration;
 import dev.valente.desafiocadastro.entidade.Pet;
 import dev.valente.desafiocadastro.util.ScannerUtils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public record GenderPet(String question) implements PetRegistrationOptions {
@@ -10,10 +11,18 @@ public record GenderPet(String question) implements PetRegistrationOptions {
     @Override
     public void registerPetInfo(Pet pet, Scanner input) {
         int c;
-        c = input.nextInt();
-        ScannerUtils.cleanBuffer(input);
-        pet.setGender(c);
-        System.out.println("Informação salva: " + pet.getGender());
+        String s = "n";
+        do{
+            try{
+                c = input.nextInt();
+                ScannerUtils.cleanBuffer(input);
+                pet.setGender(c);
+                System.out.println("Deseja salvar esta informação?(S/N): " + pet.getAddress());
+                s = input.nextLine();
+            } catch (IllegalStateException ex){
+                System.out.println(ex.getMessage());
+            }
+        }while (!s.equalsIgnoreCase("S"));
     }
 
     @Override

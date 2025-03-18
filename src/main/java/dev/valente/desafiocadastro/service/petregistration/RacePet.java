0918744369751bfdfaf76faddd75c7075b364e5c
Valente <bielvhv3@gmail.com>
2025-03-18 19:20:13 -0300

    feature: Adicionando loop para salvar informação e preenchimento default quando valor não for informado.

--- src/main/java/dev/valente/desafiocadastro/service/petregistration/RacePet.java
+++ src/main/java/dev/valente/desafiocadastro/service/petregistration/RacePet.java
@@ -8,10 +8,22 @@ public record RacePet(String question) implements PetRegistrationOptions {
 
     @Override
     public void registerPetInfo(Pet pet, Scanner input) {
-        String c;
-        c = input.nextLine();
-        pet.setRace(c);
-        System.out.println("Informação salva: " + pet.getRace());
+        String c = "n";
+        do{
+            if(c.isEmpty()){
+                pet.setRace(NAO_INFORMADO);
+                System.out.println("Raça salva como: " + NAO_INFORMADO);
+                return;
+            }
+            try{
+                c = input.nextLine();
+                assertIfMatchesRegex(c);
+                pet.setRace(c);
+                System.out.println("Deseja salvar informação?(S/N): " + pet.getRace());
+            }catch (RuntimeException e){
+                System.out.println(e.getMessage());
+            }
+        }while (!c.equalsIgnoreCase("S"));
     }
 
     @Override

813dac2b0c328368f256871002cbf9244fcadcf3
Valente <bielvhv3@gmail.com>
2025-03-19 14:52:55 -0300

    feature: Adicionando funções para arquivar pets.

--- src/main/java/dev/valente/desafiocadastro/service/petregistration/RegisterForm.java
+++ src/main/java/dev/valente/desafiocadastro/service/petregistration/RegisterForm.java
@@ -4,6 +4,11 @@ import dev.valente.desafiocadastro.entidade.Pet;
 import dev.valente.desafiocadastro.factory.PetRegistrationOptionsFactory;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -49,12 +54,52 @@ public class RegisterForm {
 //        bw.close();
 //    }
 
-    public Pet registerPet(Scanner sc){
+    public Pet registerPet(Scanner sc) throws IOException, IllegalAccessException {
         Pet newPet = new Pet();
         getMap().forEach((key, value) -> {
             value.registerPetInfo(newPet, sc);
         });
+        registerPetInFile(newPet);
         return newPet;
+    }
+
+    private void registerPetInFile(Pet pet) throws IOException, IllegalAccessException {
+        StringBuilder sb = new StringBuilder();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmm");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String now = localDateTime.format(formatter);
+        sb.append(now).append("-").append(pet.getFullName().toUpperCase().replace(" ", ""));
+        String fileName = "src/main/java/dev/valente/desafiocadastro/petsCadastrados/" + sb + ".txt";
+        File newPetFile = new File(fileName);
+        var path = Files.createFile(newPetFile.toPath());
+        preencherTxt(path, pet);
+    }
+
+    private void preencherTxt(Path petFile, Pet pet) throws IOException, IllegalAccessException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(petFile.toFile(), true));
+        Field[] fields = pet.getClass().getDeclaredFields();
+        int counter = 0;
+        for (Field field : fields) {
+            counter++;
+            field.setAccessible(true);
+            var properties = field.get(pet);
+            if(properties.getClass().getSimpleName().equalsIgnoreCase("address")){
+                Field[] addressFields = properties.getClass().getDeclaredFields();
+                StringBuilder addressBuilder = new StringBuilder();
+                for (Field addressField : addressFields) {
+                    addressField.setAccessible(true);
+                    var propertiesAddress = addressField.get(properties);
+                    addressBuilder.append(propertiesAddress.toString()).append(", ");
+                    addressField.setAccessible(false);
+                }
+                bw.write(counter + " - " + addressBuilder + "\n");
+            } else {
+                var propertie = properties.toString();
+                bw.write(counter + " - " + propertie + "\n");
+            }
+            field.setAccessible(false);
+        }
+        bw.close();
     }
 
 //    public void alterarPet(Scanner sc){

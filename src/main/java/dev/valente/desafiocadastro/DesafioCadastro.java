bc2a69a3e337b8424822f6ff7054dc41f69d35fd
Valente <bielvhv3@gmail.com>
2025-03-22 17:48:19 -0300

    refactor: Trocando ifs para estrutura switch

--- src/main/java/dev/valente/desafiocadastro/DesafioCadastro.java
+++ src/main/java/dev/valente/desafiocadastro/DesafioCadastro.java
@@ -12,55 +12,66 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DesafioCadastro {
+
+    public final static int NUMBER_OF_OPTIONS = 6;
+
     public static void main(String[] args) throws IOException, IllegalAccessException {
         Scanner sc = new Scanner(System.in);
         int input = 0;
         do{
             MenuService.showMenu();
-            System.out.print("Escolha uma opção: ");
             try{
+                System.out.print("Escolha uma opção: ");
                 input = sc.nextInt();
                 ScannerUtils.cleanBuffer(sc);
-                if(!(input >= 0)){
-                    throw new InputMismatchException();
+                if(input <= 0 || input > NUMBER_OF_OPTIONS){
+                    throw new RuntimeException();
                 }
                 System.out.println("======================================");
-                if (input == 1) {
-                    PetRegistrationOptionsRepository petRegistrationOptionsRepository =
-                            new PetRegistrationOptionsRepository();
-                    RegistrationPetService registrationPetService =
-                            new RegistrationPetService(petRegistrationOptionsRepository);
-                    Pet novoPet = registrationPetService.registerPet(sc);
-                    registrationPetService.showPet(novoPet);
-                } else if(input == 2) {
-                    PetsRepository petsRepository = new PetsRepository();
-                    SearchPetService searchPetService = new SearchPetService(petsRepository);
-                    var pets = searchPetService.getAllPets();
-                    searchPetService.showAllPets(pets);
-                    AlterationPetService alterationPetService =
-                            new AlterationPetService();
-                    alterationPetService.changePet(pets);
-                } else if(input == 3) {
-                    PetsRepository petsRepository = new PetsRepository();
-                    SearchPetService searchPetService = new SearchPetService(petsRepository);
-                    var pets = searchPetService.getAllPets();
-                    searchPetService.showAllPets(pets);
-                    DeletePetService deletePetService = new DeletePetService();
-                    deletePetService.deletePet(pets);
-                } else if(input == 4) {
-                    PetsRepository petsRepository = new PetsRepository();
-                    SearchPetService petSearch = new SearchPetService(petsRepository);
-                    var allPets = petSearch.getAllPets();
-                    petSearch.showAllPets(allPets);
-                } else if(input == 5) {
-                    PetsRepository petsRepository = new PetsRepository();
-                    SearchPetService petSearch = new SearchPetService(petsRepository);
-                    var petsEncountered = petSearch.searchPets();
-                    petSearch.showAllPets(petsEncountered);
+                switch (input){
+                    case 1 -> {
+                        PetRegistrationOptionsRepository petRegistrationOptionsRepository =
+                                new PetRegistrationOptionsRepository();
+                        RegistrationPetService registrationPetService =
+                                new RegistrationPetService(petRegistrationOptionsRepository);
+                        Pet novoPet = registrationPetService.registerPet(sc);
+                        registrationPetService.showPet(novoPet);
+                    }
+                    case 2 -> {
+                        PetsRepository petsRepository = new PetsRepository();
+                        SearchPetService searchPetService = new SearchPetService(petsRepository);
+                        var pets = searchPetService.getAllPets();
+                        searchPetService.showAllPets(pets);
+                        AlterationPetService alterationPetService =
+                                new AlterationPetService();
+                        alterationPetService.changePet(pets);
+                    }
+                    case 3 -> {
+                        PetsRepository petsRepository = new PetsRepository();
+                        SearchPetService searchPetService = new SearchPetService(petsRepository);
+                        var pets = searchPetService.getAllPets();
+                        searchPetService.showAllPets(pets);
+                        DeletePetService deletePetService = new DeletePetService();
+                        deletePetService.deletePet(pets);
+                    }
+                    case 4 -> {
+                        PetsRepository petsRepository = new PetsRepository();
+                        SearchPetService petSearch = new SearchPetService(petsRepository);
+                        var allPets = petSearch.getAllPets();
+                        petSearch.showAllPets(allPets);
+                    }
+                    case 5 -> {
+                        PetsRepository petsRepository = new PetsRepository();
+                        SearchPetService petSearch = new SearchPetService(petsRepository);
+                        var petsEncountered = petSearch.searchPets();
+                        petSearch.showAllPets(petsEncountered);
+                    }
                 }
             }catch (InputMismatchException ex){
                 ScannerUtils.cleanBuffer(sc);
-                System.out.println("Por favor insira um número válido!");
+                System.out.println("Por favor digite apenas números de 1 a 6");
+            } catch (RuntimeException ex){
+                System.out.println("Por favor insira um número válido de 1 a 6");
             }
         } while (input != 6);
 

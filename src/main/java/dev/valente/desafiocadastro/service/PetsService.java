acabc4e8871e2eb1cb11299434cd4adad39d4580
Valente <bielvhv3@gmail.com>
2025-03-23 11:53:39 -0300

    feature: Removendo classe de deleção e passando função de deleção para o petservice

--- src/main/java/dev/valente/desafiocadastro/service/PetsService.java
+++ src/main/java/dev/valente/desafiocadastro/service/PetsService.java
@@ -1,12 +1,16 @@
 package dev.valente.desafiocadastro.service;
 
 import dev.valente.desafiocadastro.repository.PetsRepository;
+import dev.valente.desafiocadastro.util.ScannerUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class PetsService {
 
@@ -62,4 +66,50 @@ public class PetsService {
     public List<File> getAllPets(){
         return petsRepository.getFilesOfRegisteredPets();
     }
+
+    public void deletePet(File fileToDelete) throws IOException {
+        var allPets = getAllPets();
+        Scanner scanner = new Scanner(System.in);
+        String inputUserString = "n";
+        do {
+            System.out.println("Tem certeza que deseja excluir este pet (S/N): ");
+            inputUserString = scanner.nextLine();
+            if (inputUserString.equalsIgnoreCase("s")) {
+                allPets.remove(fileToDelete);
+                Files.deleteIfExists(fileToDelete.toPath());
+                System.out.println("Deletado com sucesso!");
+            }
+        } while (!inputUserString.equalsIgnoreCase("s"));
+    }
+
+    public List<File> searchPets(){
+        SearchPetService searchPetService = new SearchPetService(petsRepository);
+        return searchPetService.searchPets();
+    }
+
+    public File searchPet(){
+        var allPets = getAllPets();
+        File fileToReturn = null;
+        Scanner scanner = new Scanner(System.in);
+        int inputUserInt = 0;
+        while (fileToReturn == null){
+            try{
+                System.out.println("Escolha um pet: ");
+                inputUserInt = scanner.nextInt();
+                ScannerUtils.cleanBuffer(scanner);
+                inputUserInt -= 1;
+                assertIfInIsGreaterThanSizeOfList(inputUserInt, allPets);
+                fileToReturn = allPets.get(inputUserInt);
+            } catch (InputMismatchException ex){
+                ScannerUtils.cleanBuffer(scanner);
+                System.out.println(ex.getMessage());
+            }
+        }
+        return fileToReturn;
+    }
+
+    private void assertIfInIsGreaterThanSizeOfList(int input, List<File> fileToCheck) {
+        if (input > fileToCheck.size()) throw new RuntimeException("Digite um valor dos que foram mostrados na tela");
+    }
+
 }
